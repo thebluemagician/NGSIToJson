a1d1632025266cf2d509633a473fed9fe909b780
U-NECTECHNOLOGIES\Md.Adil <Md.Adil@DLLT-9192.nectechnologies.in>
2020-09-24 02:43:31 +0530

    Normalied ngsi notification impl with or without metadata

--- nifi-custom-processors/src/main/java/org/apache/nifi/processors/custom/NGSIToJson.java
+++ nifi-custom-processors/src/main/java/org/apache/nifi/processors/custom/NGSIToJson.java
@@ -17,15 +17,13 @@ package org.apache.nifi.processors.custom;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.StringWriter;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
-import org.apache.commons.io.IOUtils;
 import org.apache.nifi.annotation.behavior.InputRequirement;
 import org.apache.nifi.annotation.behavior.InputRequirement.Requirement;
 import org.apache.nifi.annotation.behavior.ReadsAttribute;
@@ -39,6 +37,7 @@ import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.annotation.lifecycle.OnScheduled;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processor.AbstractProcessor;
 import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.ProcessSession;
@@ -48,6 +47,9 @@ import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.io.InputStreamCallback;
 import org.apache.nifi.processor.io.OutputStreamCallback;
 import org.apache.nifi.processor.util.StandardValidators;
+import org.apache.nifi.processors.custom.util.FlowFileMappper;
+import org.apache.nifi.stream.io.StreamUtils;
+import org.json.JSONObject;
 
 @SupportsBatching
 @InputRequirement(Requirement.INPUT_REQUIRED)
@@ -58,36 +60,39 @@ import org.apache.nifi.processor.util.StandardValidators;
 @WritesAttributes({@WritesAttribute(attribute = "", description = "")})
 public class NGSIToJson extends AbstractProcessor {
 
-  public static final PropertyDescriptor ATTRIBUTE = new PropertyDescriptor.Builder()
-      .name("Attributes").displayName("Comma separted attribute")
-      .description("Comma separted attribute or fields to add in Json")
-      .required(false)
-      .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-      .build();
-  
-  public static final PropertyDescriptor NGSI_VERSION = new PropertyDescriptor.Builder()
-      .name("NGSI Version")
-      .displayName("NGSI Version")
-      .description("NGSIv2 is Supported")
-      .required(false)
-      .allowableValues("v2")
-      .defaultValue("v2")
-      .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-      .build();
+  private static final String JSON_STRUCTURE_FLAT = "Flat";
+  private static final String JSON_STRUCTURE_NESTED = "Nested";
 
-  public static final Relationship REL_SUCCESS = new Relationship.Builder()
-      .name("Success")
-      .description("All created FlowFiles are routed to this relationship")
-      .build();
-  
-  public static final Relationship REL_RETRY = new Relationship.Builder()
-      .name("Retry")
-      .description("Retry flowfile operation")
-      .build();
-  public static final Relationship REL_FAILURE = new Relationship.Builder()
-      .name("Failure")
-      .description("Failure of operation")
-      .build();
+  public static final PropertyDescriptor JSON_TYPE = new PropertyDescriptor.Builder()
+      .name("Json Structure").description("Specifies whether the Json should be Flat or Nested")
+      .required(true).defaultValue(JSON_STRUCTURE_FLAT)
+      .allowableValues(JSON_STRUCTURE_FLAT, JSON_STRUCTURE_NESTED).build();
+
+  public static final PropertyDescriptor ATTRIBUTE = new PropertyDescriptor.Builder()
+      .name("Attributes")
+      .description("Comma separted attribute or fields to add in Json, By default all "
+          + "the data attributes along with if metadata is true, metadata attribute will be included")
+      .required(false).addValidator(StandardValidators.NON_EMPTY_VALIDATOR).build();
+
+  public static final PropertyDescriptor NGSI_VERSION = new PropertyDescriptor.Builder()
+      .name("NGSI Version").description("NGSIv2 is Supported").required(false).allowableValues("v2")
+      .defaultValue("v2").addValidator(StandardValidators.NON_EMPTY_VALIDATOR).build();
+
+  public static final PropertyDescriptor METADATA =
+      new PropertyDescriptor.Builder().name("Include Metadata")
+          .description("If true, the general metadata like- id, type, subscriptionId will "
+              + "be included in the FlowFile, Attributes metadata will be ignored")
+          .required(true).allowableValues("True", "False").defaultValue("False")
+          .addValidator(StandardValidators.NON_EMPTY_VALIDATOR).build();
+
+  public static final Relationship REL_SUCCESS = new Relationship.Builder().name("Success")
+      .description("All created FlowFiles are routed to this relationship").build();
+
+  public static final Relationship REL_RETRY =
+      new Relationship.Builder().name("Retry").description("Retry flowfile operation").build();
+
+  public static final Relationship REL_FAILURE =
+      new Relationship.Builder().name("Failure").description("Failure of operation").build();
 
   private List<PropertyDescriptor> descriptors;
 
@@ -95,9 +100,12 @@ public class NGSIToJson extends AbstractProcessor {
 
   @Override
   protected void init(final ProcessorInitializationContext context) {
+
     final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
+    descriptors.add(JSON_TYPE);
     descriptors.add(ATTRIBUTE);
     descriptors.add(NGSI_VERSION);
+    descriptors.add(METADATA);
     this.descriptors = Collections.unmodifiableList(descriptors);
 
     final Set<Relationship> relationships = new HashSet<Relationship>();
@@ -122,48 +130,73 @@ public class NGSIToJson extends AbstractProcessor {
 
   }
 
+  String processRequest(final ProcessContext context, Map<String, String> flowFileAttributes,
+      String flowFileContent) {
+
+    String attribute = context.getProperty(ATTRIBUTE).getValue();
+    String ngsiVersion = context.getProperty(NGSI_VERSION).getValue();
+    Boolean metaDataFlag = context.getProperty(METADATA).asBoolean();
+
+    FlowFileMappper mapper = new FlowFileMappper();
+
+    String json = mapper.attributeMapper(attribute, ngsiVersion, metaDataFlag, flowFileContent,
+        flowFileAttributes);
+
+
+    return json;
+  }
+
+
+
   @Override
   public void onTrigger(final ProcessContext context, final ProcessSession session)
       throws ProcessException {
-    
-    String attributes = context.getProperty(ATTRIBUTE).getValue();
-    String ngsiVersion = context.getProperty(NGSI_VERSION).getValue();
-    
-    FlowFile flowFile = session.get();
-    final AtomicReference<String> partValue = new AtomicReference<String>();
 
-    
+    FlowFile flowFile = session.get();
+    if (flowFile == null) {
+      return;
+    }
+
+    final ComponentLog logger = getLogger();
+    // final AtomicReference<String> partValue = new AtomicReference<String>();
+    final byte[] buffer = new byte[Math.toIntExact(flowFile.getSize())];
+
     session.read(flowFile, new InputStreamCallback() {
       @Override
       public void process(InputStream in) throws IOException {
-          try {
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(in, writer, Charset.defaultCharset());
-            partValue.set(writer.toString());
-          } catch (Exception ex) {
-              ex.printStackTrace();
-          }
+        // StringWriter writer = new StringWriter();
+        // IOUtils.copy(in, writer, Charset.defaultCharset());
+        // partValue.set(writer.toString());
+
+        StreamUtils.fillBuffer(in, buffer);
+
       }
-  });
-    
-    //flowFile = session.putAttribute(flowFile, "file.part.value", partValue.get());
-    
+    });
+
+    Map<String, String> flowFileAttributes = flowFile.getAttributes();
+    final String flowFileContent = new String(buffer, StandardCharsets.UTF_8);
+    Map<PropertyDescriptor, String> processorProperties = context.getProperties();
+
+    logger.error("NGSI-Content: " + flowFileContent);
+
+    // if(!testValue.isEmpty() || testValue != null) {
+
+    String value = processRequest(context, flowFileAttributes, flowFileContent);
+
     flowFile = session.write(flowFile, new OutputStreamCallback() {
 
       @Override
       public void process(OutputStream out) throws IOException {
-          out.write(partValue.get().getBytes());
+        logger.error("Testing NGSIToJson: " + value);
+
+        out.write(value.getBytes(StandardCharsets.UTF_8));
       }
-  });
-    
+    });
+    // session.putAllAttributes(flowFile, flowFileAttributes);
     session.transfer(flowFile, REL_SUCCESS);
-
-
-    
-    
-    if (flowFile == null) {
-      return;
-    }
-    // TODO implement
+    // }
+    // logger.error("Attributes Property is Empty");
+    // session.transfer(flowFile, REL_FAILURE);
   }
+
 }
---
+++ nifi-custom-processors/src/main/java/org/apache/nifi/processors/custom/util/FlowFileMappper.java
@@ -0,0 +1,121 @@
+package org.apache.nifi.processors.custom.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import org.apache.commons.collections4.map.CaseInsensitiveMap;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlowFileMappper {
+
+  Logger logger = LoggerFactory.getLogger(FlowFileMappper.class);
+
+  public JSONObject map2json(Map<PropertyDescriptor, String> processorProperties,
+      String ngsiFlowFile, Map<String, String> flowFileAttributes) {
+
+
+    Map<String, String> newFlowFileAttributes =
+        new CaseInsensitiveMap<String, String>(flowFileAttributes);
+
+    JSONObject test2 = new JSONObject();
+
+    for (Map.Entry<String, String> entry : newFlowFileAttributes.entrySet()) {
+      test2.put(entry.getKey(), entry.getValue());
+    }
+
+    logger.error(ngsiFlowFile);
+    /*
+     * JsonReader jsonReader = new JsonReader(new StringReader(ngsiFlowFile));
+     * jsonReader.setLenient(true); JsonElement element = JsonParser.parseReader(jsonReader);
+     */
+
+    JSONObject element = new JSONObject(ngsiFlowFile);
+    test2.put("ngsiData", element);
+    return test2;
+  }
+
+  // public JSONObject attributeMapper(Map<PropertyDescriptor, String> processorProperties,
+  // String ngsiFlowFile, Map<String, String> flowFileAttributes) {
+  //
+  // Map<String, String> generatedAttributes = new HashMap<String, String>();
+  //
+  // for (final Map.Entry<PropertyDescriptor, String> entry : processorProperties.entrySet()) {
+  // PropertyDescriptor property = entry.getKey();
+  // if (property.isDynamic() && property.isExpressionLanguageSupported()) {
+  // String dynamicValue = processorProperties.get(property);
+  // generatedAttributes.put(property.getName(), dynamicValue);
+  // }
+  // }
+  //
+  // JSONObject test2 = new JSONObject();
+  //
+  // for (Map.Entry<String, String> entry : generatedAttributes.entrySet()) {
+  // test2.put(entry.getKey(), entry.getValue());
+  // }
+  //
+  // return test2;
+  // }
+
+  public String attributeMapper(String attribute, String ngsiVersion, Boolean metaDataFlag,
+      String flowFileContent, Map<String, String> flowFileAttributes) {
+
+    JSONObject jsonFlowFile = toJson(flowFileContent);
+    JSONObject jsonData = new JSONObject();
+    JSONObject jsonMetaData = new JSONObject();
+    ArrayList<String> metaAttrs = new ArrayList<String>(
+        new ArrayList<String>(Arrays.asList("subscriptionId", "type", "id", "TimeInstant")));
+    
+    logger.error("JsonFlowFile: " + jsonFlowFile.toString());
+
+
+    for (String key : jsonFlowFile.keySet()) {
+      if (key.equals("data")) {
+        JSONObject dataObject = jsonFlowFile.getJSONArray(key).getJSONObject(0);
+
+        for (String jsonKey : dataObject.keySet()) {
+          if (metaAttrs.contains(jsonKey)) {
+            if (jsonKey.equals("TimeInstant")) {
+              jsonMetaData.put(jsonKey, dataObject.getJSONObject(jsonKey).getString("value"));
+            } else {
+              jsonMetaData.put(jsonKey, dataObject.get(jsonKey));
+            }
+          } else {
+            jsonData.put(jsonKey, dataObject.getJSONObject(jsonKey).get("value"));
+          }
+        }
+      } else if (metaAttrs.contains(key)) {
+        jsonMetaData.put(key, jsonFlowFile.get(key));
+      }
+    }
+
+    logger.error("JSON_DATA: " + jsonData.toString());
+    JSONObject processedData = new JSONObject();
+
+    if (metaDataFlag == Boolean.FALSE) {
+      processedData = jsonData;
+    } else if (metaDataFlag == Boolean.TRUE) {
+      processedData.put("data", jsonData).put("metadata", jsonMetaData).toString();
+    }
+
+    logger.error("processed data: " + processedData.toString());
+    return processedData.toString();
+  }
+
+  JSONObject toJson(String jsonString) {
+
+    JSONObject element = new JSONObject(jsonString);
+    return element;
+  }
+  
+  JSONObject jsonStructure() {
+    
+    return null;
+  }
+}
